51bb68279a140cc514ddfcc37f461d1225b77cc2
Darius.Baies <Darius.Baies@BAVE439C>
2019-08-28 16:21:22 +0300

    Added Tasks page

---
+++ Data/TasksDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Siemens.MP.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Siemens.MP.Data
+{
+    public class TasksDbContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+
+        public TasksDbContext(DbContextOptions<TasksDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Entities/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using Siemens.MP.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Siemens.MP.Entities
+{
+    public class Tasks
+    {
+        public Tasks(string taskName, string taskDescription, DateTime taskDeadline)
+        {
+            this.TaskName = taskName;
+            this.TaskDescription = taskDescription;
+            this.TaskDeadline = taskDeadline;
+            this.TaskState = StateOfTask.TO_DO;
+        }
+        public int IdTask { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskDeadline { get; set; }
+        public StateOfTask TaskState { get; set; } 
+    }
+}
