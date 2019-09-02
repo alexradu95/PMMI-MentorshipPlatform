02546e3a485a2a178452023b5f96a7abfc412e3f
Salagean Bogdan <Alexandru.Salagean@ctbav.ro>
2019-09-02 16:11:34 +0300

    created admin student and mentor roles  , added the logic behind accessing different states using different roles

---
+++ Enums/RoleType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Siemens.MP.Enums
+{
+   public enum RoleType
+    {
+      ADMIN,
+      MENTOR,
+      STUDENT
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -14,15 +14,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Siemens.MP.Areas.Identity;
 using Siemens.MP.Data;
+using Siemens.MP.Entities;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Configuration;
 
 namespace Siemens.MP
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+
 
         public IConfiguration Configuration { get; }
 
@@ -30,20 +31,77 @@ namespace Siemens.MP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+          
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddRoles<IdentityRole>()
+               .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+            
+
+
+
+        }
+        private async System.Threading.Tasks.Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles 
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] roleNames = { "Admin", "Mentor", "Student" };
+            IdentityResult roleResult;
+            
+            
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //Here you could create a super user who will maintain the web app
+            var poweruser = new IdentityUser
+            {
+
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com"
+            };
+            //Ensure you have these values in your appsettings.json file
+            string userPWD = "1q2w3e4rT!";
+            var _user = await UserManager.FindByEmailAsync("admin@gmail.com");
+        
+            if (_user == null)
+            {
+                var createPowerUser = await UserManager.CreateAsync(poweruser,userPWD);
+                if (createPowerUser.Succeeded)
+                {
+                    //here we tie the new user to the role
+                    await UserManager.AddToRoleAsync(poweruser, "Admin");
+
+                }
+            }
+            
+        }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
         }
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env , IServiceProvider serviceProvider)
         {
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -63,6 +121,7 @@ namespace Siemens.MP
 
             app.UseAuthentication();
             app.UseAuthorization();
+           
 
             app.UseEndpoints(endpoints =>
             {
@@ -70,6 +129,10 @@ namespace Siemens.MP
                 endpoints.MapBlazorHub<App>(selector: "app");
                 endpoints.MapFallbackToPage("/_Host");
             });
+            CreateRoles(serviceProvider).Wait();
+            
         }
+        
     }
+    
 }
