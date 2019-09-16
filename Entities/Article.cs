b0f0b476ce2b37e485cc5d44c0c4f18e4b7391c5
Alban Ionut <Ionut.Alban@ctbav.ro>
2019-09-16 13:09:11 +0300

    new page  knowledge database and small improvements on my profile page

---
+++ Entities/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Siemens.MP.Entities
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public Author Author { get; set; }
+    }
+}
