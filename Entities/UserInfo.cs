5f1c3fa65a0b86a2e9fa6467d613dbf6734800d6
Salagean Bogdan <Alexandru.Salagean@ctbav.ro>
2019-09-26 14:44:09 +0300

    pages can not be seen now without beeing logged in

--- Entities/UserInfo.cs
+++ Entities/UserInfo.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +16,7 @@ namespace Siemens.MP.Entities
         public string Phone { get; set; }
         public string Github { get; set; }
         public string Facebook { get; set; }
+        
+
     }
 }
